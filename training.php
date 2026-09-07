d2e746245d793bf2a431dd6adf0f3ab18444a65e
ddnator <wangdarren3@gmail.com>
2026-09-07 13:17:55 +0200

    added articles

---
+++ training.php
@@ -0,0 +1,55 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/training.css">
+</head>
+<body>
+    <nav>dew</nav>
+    <main>
+        <article>
+            <img src="images/placeholder.jpg" alt="">
+            
+            <div>
+                <button class="green-button">✓</button>
+                <button class="red-button">X</button>
+            </div>
+
+        </article>
+
+        <article>
+            <img src="images/placeholder.jpg" alt="">
+            
+            <div>
+                <button class="green-button">✓</button>
+                <button class="red-button">X</button>
+            </div>
+        </article>
+
+        <article>
+            <img src="images/placeholder.jpg" alt="">
+            
+            <div>
+                <button class="green-button">✓</button>
+                <button class="red-button">X</button>
+            </div>
+        </article>
+
+        <article><img src="images/placeholder.jpg" alt="">
+            
+            <div>
+                <button class="green-button">✓</button>
+                <button class="red-button">X</button>
+            </div></article>
+
+
+    </main>
+
+    <footer></footer>
+</body>
+</html>
