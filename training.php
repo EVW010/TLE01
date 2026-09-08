c459662fff9aab8b6b93b78b0b3256475e6bdc46
ddnator <wangdarren3@gmail.com>
2026-09-08 14:34:39 +0200

    changed music

--- training.php
+++ training.php
@@ -91,7 +91,9 @@ if (isset($_POST['submit'])) {
         </article>
 
         <article>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet commodo risus, vitae vehicula nulla eleifend non. Vestibulum viverra lectus quis enim accumsan ullamcorper. Morbi a sapien justo. Nullam sed tincidunt eros, at sollicitudin nisl. Mauris ac orci arcu. Nullam eget pellentesque diam. Sed fringilla, lacus eu viverra sodales, eros erat convallis justo, ac ultrices nunc massa sit amet dui. Quisque quis tempor arcu. Duis molestie pulvinar dui quis gravida. Maecenas a maximus tortor.
+            <p>"If you are distressed by anything external, the pain is not due to the thing itself, 
+                but to your estimate of it; and this you have the power to revoke at any moment. 
+                You have power over your mind - not outside events. Realize this, and you will find strength."
             </p>
             
             <div>
@@ -108,7 +110,7 @@ if (isset($_POST['submit'])) {
 
         <article>
             <audio controls>
-            <source src="horse.ogg" type="audio/mp3">
+            <source src="music/AI_sample.mp3" type="audio/mp3">
 
             </audio>
             
